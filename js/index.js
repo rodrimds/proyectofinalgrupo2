772c2e9628abce2ff9d9e7c4ba3044905e60a06a
Rodrigo MdS <rodrimds1996@gmail.com>
2025-08-23 00:12:33 -0300

    Actualizacion del index.js
    Se agrego DOM con una funcion que haga un check si esta una sesion iniciada

--- js/index.js
+++ js/index.js
@@ -10,5 +10,15 @@ document.addEventListener("DOMContentLoaded", function(){
     document.getElementById("muebles").addEventListener("click", function() {
         localStorage.setItem("catID", 103);
         window.location = "products.html"
-    });
+    });    
+    document.addEventListener('DOMContentLoaded', checkSession());//hace un check de session al cargar la pagina
+    
+    function checkSession() {
+        const session = sessionStorage.getItem('userSession') 
+        
+        if (!session) {
+            // Si no hay sesión, redirige al login
+            window.location.href = 'login.html';
+        }
+    }
 });
